4a0fcf0e2c1b3f70a328012b7e27b91d21285e94
Matt Lindsey <waypastspooked@gmail.com>
2018-04-09 15:37:53 -0400

    Fixed users table migration.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,6 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email');
             $table->string('password');
             $table->string('first_name');
             $table->string('last_name');
